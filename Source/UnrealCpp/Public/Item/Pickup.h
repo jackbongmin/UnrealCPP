7ea1953cc8590e4de99add0706fc5be1724d21cb
jackbongmin <rrrgk12@gmail.com>
2025-11-18 10:24:49 +0900

    1118_Pickup시 아이템 빨려들어오는것 구현

--- Source/UnrealCpp/Public/Item/Pickup.h
+++ Source/UnrealCpp/Public/Item/Pickup.h
@@ -39,10 +39,10 @@ private:
 		const FHitResult& SweepResult);
 
 	UFUNCTION()
-	void OnScaleUpdate(float Value);
+	void OnTimelineUpdate(float Value);
 
 	UFUNCTION()
-	void OnScaleFinish();
+	void OnTimelineFinish();
 
 
 protected:
@@ -76,13 +76,25 @@ protected:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Pickup")
 	float RotateSpeed = 180.0f;
 
+	// 픽업 획득 효과용 거리 보간 커브
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Pickup")
+	TObjectPtr<UCurveFloat> DistanceCurve = nullptr;
+
+	// 픽업 획득 효과용 높이 커브
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Pickup")
+	TObjectPtr<UCurveFloat> HeightCurve = nullptr;
+
 	// 픽업 획득 효과용 스케일 커브
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Pickup")
 	TObjectPtr<UCurveFloat> ScaleCurve = nullptr;
 
 	// 아이템 획득에 걸리는 시간
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Pickup")
-	float Duration = 2.0f;
+	float Duration = 0.5f;
+
+	// 아이템 획득시 튀어 오르는 높이
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Pickup")
+	float PickupHeight = 50.0f;
 
 private:
 	// 이 픽업 아이템을 먹은 액터
@@ -90,5 +102,10 @@ private:
 
 	// 획득 되었는지 여부(true면 획득 처리 중)
 	bool bPickuped = false;
+
+	// 획득했을 때 메시 위치(월드)
+	FVector PickupStartLocation;
+
+
 
 };
