a26ceb81a7898a21e55df866aa523ae95467f24c
jackbongmin <rrrgk12@gmail.com>
2025-12-04 15:26:34 +0900

    1204_아이템 판매 구현

--- Source/UnrealCpp/Private/UI/Shop/ShopItemSellWidget.cpp
+++ Source/UnrealCpp/Private/UI/Shop/ShopItemSellWidget.cpp
@@ -2,4 +2,33 @@
 
 
 #include "UI/Shop/ShopItemSellWidget.h"
+#include "UI/Inventory/InventoryDragDropOperation.h"
+#include "Player/InventoryOwner.h"
 
+bool UShopItemSellWidget::NativeOnDrop(const FGeometry& InGeometry, const FDragDropEvent& InDragDropEvent, UDragDropOperation* InOperation)
+{
+	UInventoryDragDropOperation* invenOp = Cast<UInventoryDragDropOperation>(InOperation);
+	if (invenOp)
+	{
+		// 돈을 추가해야 한다.
+		// 원래 가격의 절반만큼만 추가
+
+		if (invenOp->ItemData.IsValid())
+		{
+			int32 sellPrice = invenOp->Count * invenOp->ItemData->ItemPrice * 0.5f;
+			APawn* player = GetOwningPlayerPawn();
+
+			if (player->Implements<UInventoryOwner>())
+			{
+				// 판매액만큼 돈을 추가
+				IInventoryOwner::Execute_AddMoney(player, sellPrice);
+				return true;
+
+			}
+
+		}
+
+	}
+
+	return false;
+}
